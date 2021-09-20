06a7615e4c320990fb0eab24bf64de4b3c76ecce
Michał Rubel <contact@observer.name>
2021-09-20 16:29:31 +0200

    fix boilerplate

--- tests/Boilerplate/Domain/Best/BestDomain.php
+++ tests/Boilerplate/Domain/Best/BestDomain.php
@@ -2,7 +2,7 @@
 
 namespace MichaelRubel\EnhancedContainer\Tests\Boilerplate\Domain\Best;
 
-use MichaelRubel\EnhancedContainer\Tests\Boilerplate\Builders\Best\BestBuilder;
+use MichaelRubel\EnhancedContainer\Tests\Boilerplate\Builder\Best\BestBuilder;
 
 class BestDomain
 {
