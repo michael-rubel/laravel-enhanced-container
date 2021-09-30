c72b5fa3e0d93b43973528b04d9516dacd57bb0e
Michał Rubel <contact@observer.name>
2021-09-30 21:43:45 +0200

    more boilerplate

---
+++ tests/Boilerplate/BoilerplateServiceWithVariadicConstructor.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichaelRubel\EnhancedContainer\Tests\Boilerplate;
+
+class BoilerplateServiceWithVariadicConstructor
+{
+    /**
+     * @var mixed
+     */
+    private mixed $boilerplates;
+
+    /**
+     * @param BoilerplateInterface ...$boilerplates
+     */
+    public function __construct(
+        BoilerplateInterface ...$boilerplates
+    ) {
+        $this->boilerplates = $boilerplates;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function test(): mixed
+    {
+        return $this->boilerplates;
+    }
+}
