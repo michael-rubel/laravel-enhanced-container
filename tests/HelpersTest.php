a56f51ca36bc51f0b29c70719cc36839005abb13
Michał Rubel <contact@observer.name>
2021-10-03 15:40:45 +0200

    fulfill tests

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -7,9 +7,31 @@ use MichaelRubel\EnhancedContainer\Tests\Boilerplate\BoilerplateService;
 class HelpersTest extends TestCase
 {
     /** @test */
+    public function testCanSetBindItself()
+    {
+        bind(BoilerplateService::class)->itself();
+
+        $boilerplateOne = resolve(BoilerplateService::class);
+        $boilerplateTwo = resolve(BoilerplateService::class);
+
+        $this->assertNotSame($boilerplateOne, $boilerplateTwo);
+    }
+
+    /** @test */
     public function testCanSetSingleton()
     {
         singleton(BoilerplateService::class);
+
+        $boilerplateOne = resolve(BoilerplateService::class);
+        $boilerplateTwo = resolve(BoilerplateService::class);
+
+        $this->assertSame($boilerplateOne, $boilerplateTwo);
+    }
+
+    /** @test */
+    public function testCanSetScopedInstance()
+    {
+        scoped(BoilerplateService::class);
 
         $boilerplateOne = resolve(BoilerplateService::class);
         $boilerplateTwo = resolve(BoilerplateService::class);
