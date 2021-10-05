8cd48b6b29bc9e6b0e8cf96c7bc68724b95fac9a
Michael Rubel <contact@observer.name>
2021-10-05 08:56:00 +0200

    missing import

--- src/LecServiceProvider.php
+++ src/LecServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MichaelRubel\EnhancedContainer;
 
+use MichaelRubel\EnhancedContainer\Core\CallProxy;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
