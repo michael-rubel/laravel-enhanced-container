326b08976cb9c96b9b3f844fd9f1927732801017
Michał Rubel <contact@observer.name>
2021-10-12 18:21:58 +0200

    semantic naming

--- src/Core/CallProxy.php
+++ src/Core/CallProxy.php
@@ -17,12 +17,12 @@ class CallProxy implements Call
     /**
      * @var object
      */
-    private object $resolved;
+    private object $instance;
 
     /**
      * @var object
      */
-    private object $resolvedForwardsTo;
+    private object $forwardsTo;
 
     /**
      * CallProxy constructor.
@@ -34,13 +34,13 @@ class CallProxy implements Call
         private object | string $class,
         private array $dependencies = []
     ) {
-        $this->resolved = $this->resolvePassedClass(
+        $this->instance = $this->resolvePassedClass(
             $this->class,
             $this->dependencies
         );
 
         if (config('enhanced-container.forwarding_enabled')) {
-            $this->resolvedForwardsTo = (
+            $this->forwardsTo = (
                 new MethodForwarder(
                     $this->class,
                     $this->dependencies
@@ -82,21 +82,21 @@ class CallProxy implements Call
      */
     public function __call(string $method, array $parameters): mixed
     {
-        if (method_exists($this->resolved, $method)) {
-            return $this->containerCall($this->resolved, $method, $parameters);
+        if (method_exists($this->instance, $method)) {
+            return $this->containerCall($this->instance, $method, $parameters);
         } elseif (config('enhanced-container.forwarding_enabled')) {
-            return $this->containerCall($this->resolvedForwardsTo, $method, $parameters);
+            return $this->containerCall($this->forwardsTo, $method, $parameters);
         }
 
         throw new BadMethodCallException(sprintf(
             'Call to undefined method %s::%s()',
-            $this->resolved::class,
+            $this->instance::class,
             $method
         ));
     }
 
     /**
-     * Get the resolved service's property.
+     * Get the instance service's property.
      *
      * @param string $name
      *
@@ -105,17 +105,17 @@ class CallProxy implements Call
      */
     public function __get(string $name): mixed
     {
-        if (property_exists($this->resolved, $name)) {
-            return $this->resolved->{$name};
+        if (property_exists($this->instance, $name)) {
+            return $this->instance->{$name};
         } elseif (config('enhanced-container.forwarding_enabled')) {
-            return $this->resolvedForwardsTo->{$name};
+            return $this->forwardsTo->{$name};
         }
 
-        return $this->throwPropertyNotFoundException($name, $this->resolved);
+        return $this->throwPropertyNotFoundException($name, $this->instance);
     }
 
     /**
-     * Set the resolved service's property.
+     * Set the instance service's property.
      *
      * @param string $name
      * @param mixed  $value
@@ -124,18 +124,18 @@ class CallProxy implements Call
      */
     public function __set(string $name, mixed $value): void
     {
-        if (property_exists($this->resolved, $name)) {
-            $this->resolved->{$name} = $value;
+        if (property_exists($this->instance, $name)) {
+            $this->instance->{$name} = $value;
         } else {
             if (config('enhanced-container.forwarding_enabled')) {
-                property_exists($this->resolvedForwardsTo, $name)
-                    ? $this->resolvedForwardsTo->{$name} = $value
-                    : $this->throwPropertyNotFoundException($name, $this->resolvedForwardsTo);
+                property_exists($this->forwardsTo, $name)
+                    ? $this->forwardsTo->{$name} = $value
+                    : $this->throwPropertyNotFoundException($name, $this->forwardsTo);
 
                 return;
             }
 
-            $this->throwPropertyNotFoundException($name, $this->resolved);
+            $this->throwPropertyNotFoundException($name, $this->instance);
         }
     }
 }
