e5b6845dc7059f944efbffe473cd165fc7fda3f7
Michael Rubel <contact@observer.name>
2021-11-19 10:23:41 +0100

    update stub

--- tests/Boilerplate/BoilerplateServiceResolvesContextualInMethod.php
+++ tests/Boilerplate/BoilerplateServiceResolvesContextualInMethod.php
@@ -9,11 +9,26 @@ use MichaelRubel\EnhancedContainer\Core\CallProxy;
 class BoilerplateServiceResolvesContextualInMethod implements BoilerplateInterface
 {
     /**
+     * @param BoilerplateInterface $boilerplate
+     */
+    public function __construct(protected BoilerplateInterface $boilerplate)
+    {
+    }
+
+    /**
+     * @return object
+     */
+    public function constructorHasContextual(): object
+    {
+        return $this->boilerplate;
+    }
+
+    /**
      * @return CallProxy
      */
     public function methodHasContextual(): CallProxy
     {
-        return call(BoilerplateInterface::class);
+        return call(BoilerplateInterface::class, [], static::class);
     }
 
     /**
@@ -21,6 +36,6 @@ class BoilerplateServiceResolvesContextualInMethod implements BoilerplateInterfa
      */
     public function methodHasContextual2(): CallProxy
     {
-        return call(BoilerplateInterface::class);
+        return call(BoilerplateInterface::class, [], static::class);
     }
 }
