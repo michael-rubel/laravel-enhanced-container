b402c4d0a7bf9956481b1a993eed27e2b1798d90
Michael Rubel <contact@observer.name>
2022-01-14 12:14:15 +0100

    add tests

--- tests/BaseBindingTest.php
+++ tests/BaseBindingTest.php
@@ -5,6 +5,7 @@ namespace MichaelRubel\EnhancedContainer\Tests;
 use MichaelRubel\EnhancedContainer\Tests\Boilerplate\BoilerplateInterface;
 use MichaelRubel\EnhancedContainer\Tests\Boilerplate\BoilerplateService;
 use MichaelRubel\EnhancedContainer\Tests\Boilerplate\BoilerplateServiceWithConstructor;
+use MichaelRubel\EnhancedContainer\Tests\Boilerplate\Models\TestModel;
 
 class BaseBindingTest extends TestCase
 {
@@ -68,5 +69,28 @@ class BaseBindingTest extends TestCase
             BoilerplateServiceWithConstructor::class,
             resolve(BoilerplateInterface::class)
         );
+    }
+
+    /** @test */
+    public function testWrapsObjectsToClosure()
+    {
+        $model = app(TestModel::class);
+        bind('test')->to($model);
+        $this->assertSame($model, app('test'));
+
+        $model = app(TestModel::class);
+        bind('test')->singleton($model);
+        $this->assertSame($model, app('test'));
+
+        $model = app(TestModel::class);
+        bind('test')->scoped($model);
+        $this->assertSame($model, app('test'));
+
+        bind('closure')->to(fn () => 'content');
+        $this->assertEquals('content', app('closure'));
+
+        $model = app(TestModel::class);
+        bind('string')->to(TestModel::class);
+        $this->assertEquals($model, app('string'));
     }
 }
