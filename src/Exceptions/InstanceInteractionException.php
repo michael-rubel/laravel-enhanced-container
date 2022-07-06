1d412c4356259e4597c26388e90f2712e92b6fa6
Michael Rubél <contact@observer.name>
2022-07-06 13:47:45 +0200

    update message

--- src/Exceptions/InstanceInteractionException.php
+++ src/Exceptions/InstanceInteractionException.php
@@ -4,5 +4,5 @@ namespace MichaelRubel\EnhancedContainer\Exceptions;
 
 class InstanceInteractionException extends \Exception
 {
-    protected $message = 'You was operating on different instance previously. Check your forwarding rules.';
+    protected $message = 'You\'re trying to operate on the new instance, but you were accessing a different instance previously. Check your forwarding rules.';
 }
