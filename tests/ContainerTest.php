8c4a517293ab3107fa777187735be7c07f2e5c0d
Michael Rubél <contact@observer.name>
2022-09-13 10:35:26 +0200

    rename test file

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -10,7 +10,7 @@ use Psr\Container\ContainerExceptionInterface;
 use stdClass;
 use TypeError;
 
-class OriginalContainerTest extends TestCase
+class ContainerTest extends TestCase
 {
     protected function tearDown(): void
     {
